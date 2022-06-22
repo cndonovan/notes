6f40bad151ff6fd6adc42ffbdc0f26c7abea6f35
Chrissy Donovan <christine.n.donovan@gmail.com>
2022-06-22 14:09:17 -0700

    Add a test

---
+++ stacks/tests/StorageStack.test.js
@@ -0,0 +1,16 @@
+import { Template } from 'aws-cdk-lib/assertions';
+import { App, getStack } from '@serverless-stack/resources';
+import { test } from 'vitest';
+
+import { StorageStack } from '../StorageStack';
+
+test('Test StorageStack', () => {
+  const app = new App();
+  // WHEN
+  app.stack(StorageStack);
+  // THEN
+  const template = Template.fromStack(getStack(StorageStack));
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    BillingMode: 'PAY_PER_REQUEST',
+  });
+});
